eecf09c8b0a4189e4937bd302088ac0be0d9d244
文 善郁 <mun_sunouk@cocone.co.jp>
2022-10-20 15:36:58 +0900

    test

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 # import json
 # from os import path
 
@@ -33,7 +33,8 @@ setup(
     # author=package['author']['name'],
     # author_email=package['author']['email'],
     # license=package['license'],
-    packages=["ccdxt","ccdxt.base"],
+    # packages=["ccdxt","ccdxt.base"],
+    packages=find_packages(where='ccdxt'),
     # install_requires=['web3','eth-tester'],
     
     classifiers=[
